110ac8168fad3603adc5ba78c7ff297b99822cca
JCranky <paulo.siqueira@gmail.com>
2016-02-08 19:49:51 +0100

    adiciona uso  didático de _ (underline)

--- src/main/scala/com/jcranky/scalando/cap01/apaga-imgs.scala
+++ src/main/scala/com/jcranky/scalando/cap01/apaga-imgs.scala
@@ -4,6 +4,6 @@ import java.io._
 val arquivos = new File(".").listFiles
 val extensoesImgs = List(".jpg", ".jpeg", ".gif", ".png")
 
-def ehImagem(nomeArq: String) = extensoesImgs.exists(nomeArq.endsWith)
+def ehImagem(nomeArq: String) = extensoesImgs.exists(nomeArq.endsWith(_))
 
 arquivos.filter(arq => ehImagem(arq.getName)).foreach(_.delete)
