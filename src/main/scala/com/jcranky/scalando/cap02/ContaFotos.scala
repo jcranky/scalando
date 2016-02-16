cf8fc5e04eccb3b73b368de68e762e97f5f25c99
JCranky <paulo.siqueira@gmail.com>
2016-02-16 21:56:39 +0100

    corrige url da api do flickr para usar https, e adiciona exemplo de ContaFotos no cap02

---
+++ src/main/scala/com/jcranky/scalando/cap02/ContaFotos.scala
@@ -0,0 +1,43 @@
+package com.jcranky.scalando.cap02
+
+class Foto(val id: Long, val owner: String, val title: String, val farm: Int)
+
+object ContaFotos extends App {
+  val resultadoServico =
+    """
+      |<rsp stat="ok">
+      |<photos page="1" pages="666" perpage="100" total="66585">
+      |  <photo id="25046413926" owner="119192561@N02" secret="d5764b727f" server="1535"
+      |         farm="2" title="Scala" ispublic="1" isfriend="0" isfamily="0"/>
+      |  <photo id="24702428649" owner="130046925@N04" secret="53c63a4475" server="1467"
+      |         farm="2" title="NY" ispublic="1" isfriend="0" isfamily="0"/>
+      |  <photo id="24305307114" owner="76999786@N02" secret="af03afc09d" server="1613"
+      |         farm="2" title="Future starts slow" ispublic="1" isfriend="0" isfamily="0"/>
+      |  <photo id="25029759906" owner="124408581@N06" secret="fea9e2a565" server="1607"
+      |         farm="2" title="Elizabeth Dress &amp; Shoes_003" ispublic="1" isfriend="0"
+      |         isfamily="0"/>
+      |  <photo id="24938009862" owner="124408581@N06" secret="9a55753645" server="1554"
+      |         farm="2" title="Elizabeth Dress &amp; Shoes_004" ispublic="1" isfriend="0"
+      |         isfamily="0"/>
+      |  <photo id="24328322599" owner="79920649@N07" secret="1407ca8449" server="1453"
+      |         farm="2" title="Lonely Robot London Scala 201215" ispublic="1" isfriend="0"
+      |         isfamily="0"/>
+      |</photos>
+      |</rsp>
+    """.stripMargin
+
+  // dados fixos apenas para teste, parseamento do XML é feito em um capítulo futuro
+  def parseiaResultado(resultado: String): List[Foto] = List(
+    new Foto(25046413926l, "119192561@N02", "Scala", 2),
+    new Foto(24702428649l, "130046925@N04", "NY", 2),
+    new Foto(24305307114l, "76999786@N02", "Future starts slow", 2),
+    new Foto(25029759906l, "124408581@N06", "Elizabeth Dress &amp; Shoes_003", 2),
+    new Foto(24938009862l, "124408581@N06", "Elizabeth Dress &amp; Shoes_004", 2),
+    new Foto(24328322599l, "124408581@N06", "Lonely Robot London Scala 201215", 2)
+  )
+
+  val fotos = parseiaResultado(resultadoServico)
+  val countFotosFarm7 = fotos.count(foto => foto.farm == 2)
+
+  println(countFotosFarm7)
+}
