cd7bd665cadf09c314190e12060c51f280bb40af
JCranky <paulo.siqueira@gmail.com>
2016-03-20 15:43:25 +0100

    Adiciona mais dois exemplos de pattern matching

--- src/main/scala/com/jcranky/scalando/cap04/PatternMatching.scala
+++ src/main/scala/com/jcranky/scalando/cap04/PatternMatching.scala
@@ -11,4 +11,21 @@ object PatternMatching extends App {
     case VIDEOS => println("processando videos")
     case _ => println("processando qualquer outra coisa")
   }
+
+  // expressão
+  val texto = midia match {
+    case FOTOS => "processando fotos"
+    case VIDEOS => "processando videos"
+    case _ => "processando qualquer outra coisa"
+  }
+  println(texto)
+
+  // inferência de tipos
+  val resultado = midia match {
+    case FOTOS => "processando fotos"
+    case VIDEOS => "processando videos"
+    case _ => -1
+  }
+  println(resultado)
+
 }
