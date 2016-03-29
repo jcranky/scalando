80e19de201e943ee8ca7a3d89dd7babea467089f
JCranky <paulo.siqueira@gmail.com>
2016-03-29 22:24:44 +0200

    Adiciona exemplos do método apply no cap 04

--- src/main/scala/com/jcranky/scalando/cap04/ApplyUnapply.scala
+++ src/main/scala/com/jcranky/scalando/cap04/ApplyUnapply.scala
@@ -5,5 +5,16 @@ object ApplyUnapply extends App {
 
   object Foto {
     def unapply(foto: Foto): Option[(Int, String, String, Int)] = Some((foto.id, foto.owner, foto.title, foto.farm))
+    def apply(id: Int, owner: String, title: String, farm: Int) = new Foto(id, owner, title, farm)
   }
+
+  val foto = Foto(1, "jcranky", "foto do jcranky", 7)
+//  val foto = Foto.apply(1, "jcranky", "foto do jcranky", 7)
+
+  val foto1 = Foto(1, "jcranky", "foto do jcranky", 7)
+  val foto2 = Foto(2, "jcranky", "foto do jcranky", 7)
+  val foto3 = Foto(3, "jcranky", "foto do jcranky", 7)
+
+  val fotos = List(foto1, foto2, foto3)
+  val fotoUm = fotos(0)
 }
