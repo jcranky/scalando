5e7da2379fcfbed380515594c1ae72c0598ea644
JCranky <paulo.siqueira@gmail.com>
2016-05-02 22:21:30 +0200

    Adiciona exemplos de pattern matching com List no cap 06

--- src/main/scala/com/jcranky/scalando/cap06/Lists.scala
+++ src/main/scala/com/jcranky/scalando/cap06/Lists.scala
@@ -15,4 +15,29 @@ object Lists extends App {
 
   val fotos4 = fotos :+ foto2
   val fotos5 = foto2 +: fotos
+
+  fotos match {
+    case head :: tail => println(head)
+    case _ =>
+  }
+
+  fotos match {
+    case head :: Nil => println(head)   // ::
+    case Nil =>
+  }
+
+  def printaLista(lista: List[Foto]): Unit = {
+    lista match {
+      case head :: Nil =>
+        println(head)         // ::
+
+      case head :: tail =>    // ::
+        println(head)
+        printaLista(tail)
+
+      case Nil =>
+    }
+  }
+
+  printaLista(fotos)
 }
