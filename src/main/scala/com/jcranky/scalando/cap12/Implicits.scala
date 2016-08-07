4397e6cf05cc75dbddcbf5ba77c6955d8703abc9
JCranky <paulo.siqueira@gmail.com>
2016-08-07 10:47:53 +0200

    Adiciona exemplo de conversão implícita

--- src/main/scala/com/jcranky/scalando/cap12/Implicits.scala
+++ src/main/scala/com/jcranky/scalando/cap12/Implicits.scala
@@ -1,5 +1,15 @@
 package com.jcranky.scalando.cap12
 
+import com.jcranky.scalando.cap11.Foto
+
 object Implicits extends App {
   "99".toInt
+
+  implicit def toPrintableFoto(foto: Foto): PrintableFoto = new PrintableFoto(foto)
+
+  Foto(1234, "jcranky", 1234, "Foto 1").print
+}
+
+class PrintableFoto(foto: Foto) {
+  def print = println(s"printando foto [$foto]...")
 }
