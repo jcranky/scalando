de1c2f6995f22d954e4c066b5e218b746a197235
JCranky <paulo.siqueira@gmail.com>
2016-08-10 08:22:01 +0200

    Adiciona exemplo com parâmetros implícitos

---
+++ src/main/scala/com/jcranky/scalando/cap12/ParamImplicitos.scala
@@ -0,0 +1,29 @@
+package com.jcranky.scalando.cap12
+
+import com.jcranky.scalando.cap11.{Foto, ResponseParser, XMLParser}
+
+import scala.io.Source
+
+object ParamImplicitos extends App {
+  val apiKey = "flickr-api-key"
+
+  def buscaFotosComImplicits(tag: Option[String])(
+    implicit parser: ResponseParser): Seq[Foto] = {
+
+    val method = "flickr.photos.search"
+    val tagText = tag.map("&tags=" + _).getOrElse("")
+
+    val params = s"?method=$method&per_page=10&api_key=$apiKey$tagText"
+    val url =
+      s"https://api.flickr.com/services/rest/$params"
+
+    parser.parse(Source.fromURL(url).mkString)
+  }
+
+
+  implicit val parser = new XMLParser()
+
+  val fotos = buscaFotosComImplicits(None)
+  fotos foreach println
+
+}
