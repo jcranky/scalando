503256b0d7b421f9dc9a9d7bee01947f3e25a32b
JCranky <paulo.siqueira@gmail.com>
2016-08-21 16:21:18 +0200

    Adiciona primeiros elementos da API completa do flickr

---
+++ src/main/scala/com/jcranky/flickr/FlickrClient.scala
@@ -0,0 +1,13 @@
+package com.jcranky.flickr
+
+import com.typesafe.config.Config
+
+class FlickrClient {
+
+}
+
+object FlickrClient {
+  def fromConfig(config: Config): FlickrClient = {
+    ???
+  }
+}
---
+++ src/main/scala/com/jcranky/model/medias.scala
@@ -0,0 +1,8 @@
+package com.jcranky.model
+
+sealed trait Media {
+  def id: Int
+}
+
+case class Foto(id: Int)
+case class Video(id: Int)
