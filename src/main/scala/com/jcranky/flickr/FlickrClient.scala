2a80c2d2b32de2ea6e014fb228220eaf7432e9cb
JCranky <paulo.siqueira@gmail.com>
2016-08-31 08:09:02 +0200

    Adiciona esqueleto para o futuro FlickrClient completo

--- src/main/scala/com/jcranky/flickr/FlickrClient.scala
+++ src/main/scala/com/jcranky/flickr/FlickrClient.scala
@@ -2,12 +2,14 @@ package com.jcranky.flickr
 
 import com.typesafe.config.Config
 
-class FlickrClient {
-
+class FlickrClient(apiKey: String) {
+  // TODO: implement stuff !
 }
 
 object FlickrClient {
   def fromConfig(config: Config): FlickrClient = {
-    ???
+    val apiKey = config.getString("flickr.api.key")
+
+    new FlickrClient(apiKey)
   }
 }
---
+++ src/test/scala/com/jcranky/flickr/FlickrClientSpec.scala
@@ -0,0 +1,17 @@
+package com.jcranky.flickr
+
+import com.typesafe.config.{ConfigException, ConfigFactory}
+import org.specs2.mutable.Specification
+
+class FlickrClientSpec extends Specification {
+  "FlickrClient.fromConfig" should {
+    "work with valid configuration" in {
+      val client = FlickrClient.fromConfig(ConfigFactory.load("app-test.conf"))
+      client !=== null
+    }
+
+    "fail if some configuration is missing" in {
+      FlickrClient.fromConfig(ConfigFactory.load("wrong.conf")) should throwAn[ConfigException]
+    }
+  }
+}
