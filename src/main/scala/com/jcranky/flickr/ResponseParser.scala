563511bcbe9a328135fd235eb55012bac21516f7
JCranky <paulo.siqueira@gmail.com>
2016-09-11 08:34:43 +0200

    Add all fields for a Foto and add a couple more TODOs
    The project now compiles again, with a broken (on purpose) test.

--- src/main/scala/com/jcranky/flickr/ResponseParser.scala
+++ src/main/scala/com/jcranky/flickr/ResponseParser.scala
@@ -6,7 +6,10 @@ import com.typesafe.config.Config
 sealed trait ResponseParser {
   def parse(str: String): Seq[Foto]
 }
-// TODO: criar um fromConfig no object ResponseParser que usa uma config para escolher Xml vs. Json parsing
+
+// TODO: create a 'fromConfig' in the object ResponseParser that uses config information to choose Xml vs. Json parsing
+// TODO: model error codes (https://www.flickr.com/services/api/flickr.photos.search.html)
+// TODO: probably should have generic Response models, to be used by the specific parser's implementations
 
 final class XmlFlickrParser extends ResponseParser {
   override def parse(str: String): Seq[Foto] = Nil
--- src/main/scala/com/jcranky/flickr/model/medias.scala
+++ src/main/scala/com/jcranky/flickr/model/medias.scala
@@ -1,17 +1,18 @@
 package com.jcranky.flickr.model
 
-// TODO: check the field type with the flickr documentation
 sealed trait Media {
   def id: String
   def owner: String
   def secret: String
   def server: String
-  def farm: String
+  def farm: Int
   def title: String
-  def ispublic: String
-  def isfriend: String
-  def isfamily: String
+  def public: Boolean
+  def friend: Boolean
+  def family: Boolean
 }
 
-final case class Foto(id: String) extends Media
-final case class Video(id: String) extends Media
+final case class Foto(id: String, owner: String, secret: String, server: String, farm: Int, title: String, public: Boolean, friend: Boolean, family: Boolean) extends Media
+
+// TODO: add Video?
+//final case class Video(id: String) extends Media
