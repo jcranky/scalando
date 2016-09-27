5861d304dd50531264cb50402369930e7a0fd068
JCranky <paulo.siqueira@gmail.com>
2016-09-27 07:58:05 +0200

    Add test for the FlickrClient.buscaFotos

--- build.sbt
+++ build.sbt
@@ -5,9 +5,12 @@ scalaVersion := "2.11.8"
 
 scalacOptions in Test ++= Seq("-Yrangepos")
 
+val specs2Version = "3.8.5"
+
 libraryDependencies ++= Seq(
   "com.typesafe" % "config" % "1.3.0",
   "joda-time" % "joda-time" % "2.9.4",
   "org.scala-lang.modules" % "scala-xml_2.11" % "1.0.5",
-  "org.specs2" %% "specs2-core" % "3.8.4" % "test"
+  "org.specs2" %% "specs2-core" % specs2Version % "test",
+  "org.specs2" %% "specs2-mock" % specs2Version % "test"
 )
--- src/test/scala/com/jcranky/flickr/FlickrClientSpec.scala
+++ src/test/scala/com/jcranky/flickr/FlickrClientSpec.scala
@@ -1,9 +1,12 @@
 package com.jcranky.flickr
 
+import com.jcranky.flickr.HttpClient.GetResponse
+import com.jcranky.flickr.model.Foto
 import com.typesafe.config.{ConfigException, ConfigFactory}
+import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 
-class FlickrClientSpec extends Specification {
+class FlickrClientSpec extends Specification with Mockito {
   "FlickrClient.fromConfig" should {
     "work with valid configuration" in {
       val client = FlickrClient.fromConfig(ConfigFactory.load("app-test.conf"))
@@ -17,7 +20,16 @@ class FlickrClientSpec extends Specification {
 
   "FlickrClient.buscaFotos" should {
     "ask the httpclient for the photos and pass the response to the response parser" in {
-      pending // TODO: implement this test
+      val httpClient = mock[HttpClient]
+      val parser = mock[ResponseParser]
+
+      httpClient.get(any[String]) returns Right(GetResponse(200, "fotos-xml-here"))
+      parser.parse(any[String]) returns List(Foto("1", "jcranky", "123", "321", 1, "my pic", true, false, false))
+
+      val client = new FlickrClient("api-key", "base-url", httpClient, parser)
+      client.buscaFotos(List("scala"))
+
+      there was one(httpClient).get(anyString)
     }
   }
 }
