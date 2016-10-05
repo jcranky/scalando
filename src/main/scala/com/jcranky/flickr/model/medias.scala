644de3e01eb331646db06efbdde61c2537fe2f5e
JCranky <paulo.siqueira@gmail.com>
2016-10-05 07:55:45 +0200

    Add Video case class

--- src/main/scala/com/jcranky/flickr/model/medias.scala
+++ src/main/scala/com/jcranky/flickr/model/medias.scala
@@ -13,6 +13,4 @@ sealed trait Media {
 }
 
 final case class Foto(id: String, owner: String, secret: String, server: String, farm: Int, title: String, public: Boolean, friend: Boolean, family: Boolean) extends Media
-
-// TODO: add Video?
-//final case class Video(id: String) extends Media
+final case class Video(id: String, owner: String, secret: String, server: String, farm: Int, title: String, public: Boolean, friend: Boolean, family: Boolean) extends Media
